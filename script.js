71aad9d016771e14da05341eacd204f137633fa2
Adeilton Francisco da Silva <adeiltonsillva65@gmail.com>
2023-03-18 11:22:07 -0300

    close Modal
    Colocando uma função no cancel

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let modalQt = 1
 const c = (el)=> document.querySelector(el); 
 const cs = (el)=> document.querySelectorAll(el); 
 
+// Listagem das Pizzas 
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
@@ -30,7 +31,7 @@ pizzaJson.map((item, index)=>{
 
       c('.pizzaInfo--qt').innerHTML = modalQt;
 
-      c('.pizzaWindowArea').style.opacity = 0
+      c('.pizzaWindowArea').style.opacity = 0;
       c('.pizzaWindowArea').style.display = 'flex';
       setTimeout(()=>{
         c('.pizzaWindowArea').style.opacity = 1
@@ -38,4 +39,16 @@ pizzaJson.map((item, index)=>{
     });
     
     c('.pizza-area').append( pizzaItem );
+});
+
+
+// Eventos do Modal 
+function closeModal() {
+  c('.pizzaWindowArea').style.opacity = 0;
+  setTimeout(()=>{
+    c('.pizzaWindowArea').style.display = 'none';
+  }, 500);
+}
+cs(".pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton").forEach((item)=>{
+  item.addEventListener('click', closeModal);
 });
